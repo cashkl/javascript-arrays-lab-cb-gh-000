aee94655a24636a35948c42495965d7e84616b6a
Murat Celik <cashkl@yandex.com>
2018-04-26 20:51:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 kittens = ["Milo","Otis","Garfield"]
 function destructivelyAppendKitten(name)  {
-  kittens[kittens.length]="kitten"
+  kittens[kittens.length]=name
 
 }
